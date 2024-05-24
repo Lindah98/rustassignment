740b8e7f197b708cd3b74f73ffe6d521ed7a33a5
Lindah98 <lindahmugambi46@gmail.com>
2024-05-24 15:54:44 +0300

    init commit

--- assignm/src/main.rs
+++ assignm/src/main.rs
@@ -1,6 +1,25 @@
+use std::io::{self, Read};
+
 fn main() {
-    for i in(0..=50).rev()
-    {
-    println!("{},", i);
+  let mut input_str = String::new();
+  println!("Enter an integer: ");
+
+  // Read user input into a String
+  io::stdin().read_to_string(&mut input_str).expect("Error reading input");
+
+  // Parse the String into an integer
+  let mut input: i32 = match input_str.trim().parse() {
+    Ok(num) => num,
+    Err(_) => {
+      println!("Invalid input. Please enter an integer.");
+      return;
     }
+  };
+
+
+
+  // Print from 0 to input using a for loop
+  for i in 0..=input {
+    println!("{}", i);
+  }
 }
